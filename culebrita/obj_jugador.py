240863c6e69029f918484777464f82314f1d4057
Yeison Mosquera <142964641+CtrlJason@users.noreply.github.com>
2024-08-21 04:28:30 -0500

    Arreglo de la mecanica de comer
    - Se corrigio la eliminacion de espacios
    - Se corrigio el tamaño de la serpiente al comer
    - Se optimizaron algunas partes del codigo

--- culebrita/obj_jugador.py
+++ culebrita/obj_jugador.py
@@ -1,7 +1,6 @@
 #Modulos
 import mundo, obj_comida, vista_juego, time, keyboard # Para pausar el tiempo de ejecucion de una seccion del codigo
 # Variables
-
 jugador = mundo.mapa_culebrita.tablero
 class Personaje():
     def __init__(self, sprite):
@@ -11,73 +10,74 @@ class Personaje():
         pasos_x = 2
         pasos_y = 4
         ultima_tecla = "d"
+        count = 0
         puntos = 0
-        count = 1
         count_s = 1
-        count_s = 2
+        puntos_anteriores = 0
+        list_pos_x = []
+        list_pos_y = []
         while True:
-            # Verificador de presion de tecla
+            # Se sobre escriben los cuadros blancos de la serpiente con azules desde su ultima posicion
+            if puntos == 0: # [CONDUNCIONAL 1]
+                # se establece el largo de la serpiente inicial al comer la fruta
+                list_pos_y.append(pasos_y)
+                list_pos_x.append(pasos_x)
+                if count > 0: # [CONDUNCIONAL 2]
+                    list_pos_x.pop(0)
+                    list_pos_y.pop(0)
+                w_line = jugador[pasos_y][pasos_x] = "🟦"
+            if puntos != puntos_anteriores: # Este condicional se ejecuta unicamente cuando el jugador alla obtenido un punto # [CONDUNCIONAL 3]
+                list_pos_x.append(pasos_x)
+                list_pos_y.append(pasos_y)
+            if puntos > 0: # [CONDUNCIONAL 4]
+                # se agregan nuevas posiciones y se eliminan las anteriores
+                list_pos_x.append(pasos_x)
+                list_pos_y.append(pasos_y)
+                list_pos_x.pop(0)
+                list_pos_y.pop(0)
+                w_line = jugador[list_pos_y[0]][list_pos_x[0]] = "🟦"
+                w_line
+            # guardamos el puntaje actual del jugador para que no se ejecute el condicional [CONDUNCIONAL 3]
+            puntos_anteriores = puntos
+            print(list_pos_y, list_pos_x)
+            count += 1
             if keyboard.is_pressed("w") and not keyboard.is_pressed("s") and not ultima_tecla == "s": # Movimiento arriba
                 pasos_y -= 1
                 pasos_x += 0
                 ultima_tecla = "w"
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y+count_s][pasos_x] = "🟦"
-                if count_s > 1 and ultima_tecla == "a":
-                    jugador[pasos_y+1][pasos_x-1] = "🟦"
-                if count_s > 1 and ultima_tecla == "d":
-                    jugador[pasos_y+1][pasos_x+1] = "🟦"
             elif keyboard.is_pressed("d") and not keyboard.is_pressed("a") and not ultima_tecla == "a": # Movimiento derecha
                 ultima_tecla = "d"
                 pasos_y -= 0
                 pasos_x += 1
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y][pasos_x-count_s] = "🟦"
-                if count_s > 1 and ultima_tecla == "w":
-                    jugador[pasos_y-1][pasos_x-1] = "🟦"
-                if count_s > 1 and ultima_tecla == "s":
-                    jugador[pasos_y+1][pasos_x-1] = "🟦"
             elif keyboard.is_pressed("s") and not keyboard.is_pressed("w") and not ultima_tecla == "w": # Movimiento abajo
                 pasos_y += 1
                 pasos_x += 0
                 ultima_tecla = "s"
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y-count_s][pasos_x] = "🟦"
-                if count_s > 1 and ultima_tecla == "a":
-                    jugador[pasos_y-1][pasos_x-1] = "🟦"
-                if count_s > 1 and ultima_tecla == "d":
-                    jugador[pasos_y-1][pasos_x+1] = "🟦"
             elif keyboard.is_pressed("a") and not keyboard.is_pressed("d") and not ultima_tecla == "d": # Movimiento izquierda
                 pasos_y += 0
                 pasos_x -= 1
                 ultima_tecla = "a"
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y][pasos_x+count_s] = "🟦"
-                if count_s > 1 and ultima_tecla == "w":
-                    jugador[pasos_y-1][pasos_x+1] = "🟦"
-                if count_s > 1 and ultima_tecla == "s":
-                    jugador[pasos_y+1][pasos_x+1] = "🟦"
             # Verificador de ultima tecla presionada    
             if ultima_tecla == "w" and not keyboard.is_pressed("w"): # Movimiento arriba
                 pasos_x += 0
                 pasos_y -= 1
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y+count_s][pasos_x] = "🟦"
             elif ultima_tecla == "d" and not keyboard.is_pressed("d"): # Movimiento derecha
                 pasos_y -= 0
                 pasos_x += 1
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y][pasos_x-count_s] = "🟦"
             elif ultima_tecla == "s" and not keyboard.is_pressed("s"): # Movimiento abajo
                 pasos_y += 1
                 pasos_x += 0
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y-count_s][pasos_x] = "🟦"
             elif ultima_tecla == "a" and not keyboard.is_pressed("a"): # Movimiento izquierda
                 pasos_y += 0
                 pasos_x -= 1
                 jugador[pasos_y][pasos_x] = culebrita.sprite
-                jugador[pasos_y][pasos_x+count_s] = "🟦"
             if pasos_y == 8 or pasos_y == 0 or pasos_x == 8 or pasos_x == 0:
                 print("Has perdido")
                 jugador[pasos_y][pasos_x] = "🟥" 
@@ -89,6 +89,7 @@ class Personaje():
                 obj_comida.manzana.posicion_fruta()
                 puntos += 1
                 count_s += 1
+                # Al hacer mas grande la lista el tamaño de la serpiente aumenta
             vista_juego.vista()
             # Puntaje
             print(f"Puntos: {puntos}")
